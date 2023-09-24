0f639804df67d45e7cbc20c1147e745e75b8d518
Dimitri Sabadie <dimitri.sabadie@gmail.com>
2023-09-24 19:11:59 +0200

    Add backend and core crates’ skeletons.

--- piksels/src/lib.rs
+++ piksels/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait System {
+  type Framebuffer;
+  type Shader;
+  type VertexArray;
+  type Texture;
+  type Pipeline;
 }
